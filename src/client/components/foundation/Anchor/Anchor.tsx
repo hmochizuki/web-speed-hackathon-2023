5bdaf10e7cb9bc6b91002d40c2a1217a5d333b52
hi mochizuki <hi.mochizuki@buysell-technologies.com>
2025-02-02 16:42:37 +0900

    アプリ内遷移をソフトナビゲーションに変更

--- src/client/components/foundation/Anchor/Anchor.tsx
+++ src/client/components/foundation/Anchor/Anchor.tsx
@@ -1,11 +1,12 @@
 import type { ComponentProps, FC } from 'react';
+import { Link } from 'react-router-dom';
 
 import * as styles from './Anchor.styles';
 
-type Props = Omit<ComponentProps<'a'>, 'className'>;
+type Props = Omit<ComponentProps<typeof Link>, 'to'> & { href: string };
 
 export const Anchor: FC<Props> = ({ children, href, ...rest }) => (
-  <a className={styles.container()} href={href} {...rest}>
+  <Link className={styles.container()} {...rest} to={href}>
     {children}
-  </a>
+  </Link>
 );
